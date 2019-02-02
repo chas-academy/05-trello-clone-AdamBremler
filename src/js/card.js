f6bcb2b3dcaa464bb75bf96908e87f6efcbf1f34
AdamBremler <adambremler@hotmail.com>
2019-02-02 16:47:37 +0100

    Add drag and drop functionality to cards

--- src/js/card.js
+++ src/js/card.js
@@ -4,7 +4,22 @@ require('webpack-jquery-ui');
 $.widget('jtrello.card', {
     options: {},
   
-    _create: function() {},
+    _create: function() {
+        this.element.draggable({
+            helper: 'clone',
+            appendTo: 'body',
+            revert: function(event, ui) {
+                return !event;
+            },
+            start: function(event, ui) {
+                $('.ui-draggable-dragging').width($(this).width());
+                $(this).hide();
+            },
+            stop: function(event, ui) {
+                $(this).show();
+            }
+        });
+    },
 
     _destroy: function() {
         this.element.remove();
--- src/js/list.js
+++ src/js/list.js
@@ -4,7 +4,14 @@ require('webpack-jquery-ui');
 $.widget('jtrello.list', {
     options: {},
   
-    _create: function() {},
+    _create: function() {
+        this.element.droppable({
+            accept: '.card',
+            drop: function(event, ui) {
+                $(ui.draggable).prependTo($(this).find('.list-cards'));
+            }
+        });
+    },
 
     _destroy: function() {
         this.element.parent().remove();
